07d75f95d4e08aea6fb16bd4b8ee9efd794cc0b1
Darion Kellar <credkellar@gmail.com>
2026-06-19 16:33:38 -0700

    Reorganize module declarations in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,2 @@
-pub mod auth;
-pub mod error;
-pub mod flow;
-pub mod spark;
-pub mod ai_mode;
-
-pub use auth::AuthClient;
-pub use error::{GoogleSuiteError, Result};
+pub mod auth; pub mod flow; pub mod spark; 
+pub mod ai_mode; pub mod telemetry; pub mod error;
