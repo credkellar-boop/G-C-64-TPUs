ff25754223d77e6d3c3b30b94773fb9ef4b4dd4f
Darion Kellar <credkellar@gmail.com>
2026-06-21 12:30:54 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,3 @@ pub mod error;
 pub mod spark;
 pub mod auth;
 
-// 1. Re-export the error so `crate::GoogleSuiteError` works anywhere
-pub use error::GoogleSuiteError;
-
-// 2. Define the custom Result alias so `crate::Result<T>` works anywhere
-pub type Result<T> = std::result::Result<T, GoogleSuiteError>;
