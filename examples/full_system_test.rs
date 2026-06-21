48190a10fe6ad00a84d8cce5d329755027ef81cd
Darion Kellar <credkellar@gmail.com>
2026-06-21 13:03:09 -0700

    Update full_system_test.rs

--- examples/full_system_test.rs
+++ examples/full_system_test.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
-async fn main() -> crate::Result<()> {
-    let auth = crate::auth::AuthClient::new()?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let auth = google_suite_rs::auth::AuthClient::new()?;
     println!("System ready with key: {}", auth.api_key);
     Ok(())
 }
