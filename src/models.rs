fa4c877cb4c77f0e05c453f3d07ba65003849dfa
Darion Kellar <credkellar@gmail.com>
2026-06-21 13:27:22 -0700

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 
+pub mod flow_config;
+pub mod flow_response;
+pub mod spark_task;
+
 #[derive(Serialize, Deserialize)]
 pub struct GoogleResponse {
     pub status: String,
