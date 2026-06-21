f19406aa70d7f3efb94ea94f1220fee7f8cea53c
Darion Kellar <credkellar@gmail.com>
2026-06-21 13:07:01 -0700

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,14 @@
 #[derive(Debug)]
 pub enum GoogleSuiteError {
     AuthError(String),
-    // You can add more error variants here later
+    RateLimitExceeded,
 }
 
 impl std::fmt::Display for GoogleSuiteError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             GoogleSuiteError::AuthError(msg) => write!(f, "Authentication Error: {}", msg),
+            GoogleSuiteError::RateLimitExceeded => write!(f, "Rate Limit Exceeded"),
         }
     }
 }
